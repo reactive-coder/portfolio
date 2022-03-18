9ee1d74084feb8779dc92c04ee279096a580fb87
devarshpatel <dp3324@nyu.edu>
2022-03-17 22:09:52 -0400

    reframed empty div.

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -45,7 +45,7 @@ const NavBar: Function = () => {
     const [showSideBar, setShowSideBar] = useState(false)
 
     return (
-        <nav className="font-sans w-full sticky top-0 z-20 before:-z-10 before:bg-black before:opacity-20 before:absolute before:top-0 before:h-full before:w-full text-gray-50">
+        <nav className="font-sans w-full sticky top-0 z-20 before:-z-20 before:bg-black before:opacity-20 before:absolute before:top-0 before:h-full before:w-full text-gray-50">
             <Container>
                 <div className="flex py-6 items-center justify-between">
                     <span className="text-2xl tracking-wider uppercase">{home.name}</span>
@@ -69,17 +69,21 @@ const NavBar: Function = () => {
                 </div>
             </Container>
             {showSideBar ?
-                <div className="h-screen fixed top-0 left-0 bg-gray-700 flex flex-col">
-                    <span className="pl-5 my-4 pr-20 text-2xl tracking-wider uppercase">{home.name}</span>
-                    {navData.map((item, i) => (
-                        <Link key={i.toString()}
-                              className="ml-5 py-2 z-20 text-lg font-light tracking-wider hover:text-gray-400 border-b hover:border-gray-400"
-                              to={item.link}
-                              onClick={() => setShowSideBar(!showSideBar)}>
-                            <FontAwesomeIcon icon={item.icon} className="mr-2.5"/>
-                            {item.text}
-                        </Link>
-                    ))}
+                <div className="fixed top-0">
+                    <div className="h-screen w-screen bg-black opacity-70"
+                        onClick={() => setShowSideBar(!showSideBar)}/>
+                    <div className="h-screen absolute top-0 bg-gray-700 flex flex-col">
+                        <span className="pl-5 my-4 pr-12 md:pr-20 text-2xl tracking-wider uppercase">{home.name}</span>
+                        {navData.map((item, i) => (
+                            <Link key={i.toString()}
+                                  className="ml-5 py-2 z-20 text-lg font-light tracking-wider hover:text-gray-400 border-b hover:border-gray-400"
+                                  to={item.link}
+                                  onClick={() => setShowSideBar(!showSideBar)}>
+                                <FontAwesomeIcon icon={item.icon} className="mr-2.5"/>
+                                {item.text}
+                            </Link>
+                        ))}
+                    </div>
                 </div>: <div/>}
         </nav>
     )
